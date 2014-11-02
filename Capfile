353627ff204fcd63e6d93e5f844c92acf6c23dee
Julian Fischer <fischer@avarteq.de>
2014-11-02 11:15:24 +0100

    Changes ruby version in Capfile to 2.1.2; deactivates carrierwave initializer

--- Capfile
+++ Capfile
@@ -30,7 +30,7 @@ require 'capistrano/rails'
 # If you are using rbenv add these lines:
 require 'capistrano/rbenv'
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.0.0-p576'
+set :rbenv_ruby, '2.1.2'
 
 # If you are using rvm add these lines:
 # require 'capistrano/rvm'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-require 'carrierwave'
-require 'a9s_swift'
-
-BUCKET_NAME = "images"
-
-if Rails.env.production?
-  begin
-    Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
-  rescue SSHKit::Runner::ExecuteError => e
-    puts "Couldn't connect to SWIFT: #{e.inspect}"
-  end 
-end
+# require 'carrierwave'
+# require 'a9s_swift'
+#
+# BUCKET_NAME = "images"
+#
+# if Rails.env.production?
+#   begin
+#     Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
+#   rescue SSHKit::Runner::ExecuteError => e
+#     puts "Couldn't connect to SWIFT: #{e.inspect}"
+#   end
+# end
