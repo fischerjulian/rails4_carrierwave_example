7f92f1673767fedf07cad1a8c2f996d7d4a97eca
Julian Fischer <fischer@avarteq.de>
2014-11-03 16:08:48 +0100

    Removes false rescue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,5 @@ require 'a9s_swift'
 BUCKET_NAME = "images"
 
 if Rails.env.production?
-  begin
-    Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
-  rescue SSHKit::Runner::ExecuteError => e
-    puts "Couldn't connect to SWIFT: #{e.inspect}"
-  end
+  Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
 end
